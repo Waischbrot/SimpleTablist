ccc85f82f672ea426c9e6fe4b1e16bab5588c4a4
Lukas <rubymc@gmx.de>
2022-06-09 09:39:03 +0200

    implemented array to list parser

---
+++ src/main/java/de/waischbrot/simpletablist/utils/Utils.java
@@ -0,0 +1,10 @@
+package de.waischbrot.simpletablist.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+    public static List<String> stringListOf(String... args) {
+        return (Arrays.asList(args));
+    }
+}
