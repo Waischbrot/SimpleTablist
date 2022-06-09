eb577fd14cd51a9c256ea4ec8a421f62dc829309
Lukas <rubymc@gmx.de>
2022-06-09 09:42:43 +0200

    Implemented JoinListener

--- src/main/java/de/waischbrot/simpletablist/MainListener.java
+++ src/main/java/de/waischbrot/simpletablist/MainListener.java
@@ -1,4 +1,19 @@
 package de.waischbrot.simpletablist;
 
-public class MainListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class MainListener implements Listener {
+
+    private final Main plugin;
+
+    public MainListener(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        plugin.getTabListManager().sendTab(event.getPlayer());
+    }
 }
