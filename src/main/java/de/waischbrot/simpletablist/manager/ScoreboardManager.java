07fc20332103f3c48e2d7339f4d5010fb106747e
Waischbrot <RubyMC@gmx.de>
2022-07-03 12:09:15 +0200

    current

--- src/main/java/de/waischbrot/simpletablist/manager/ScoreboardManager.java
+++ src/main/java/de/waischbrot/simpletablist/manager/ScoreboardManager.java
@@ -1,12 +1,15 @@
 package de.waischbrot.simpletablist.manager;
 
 import de.waischbrot.simpletablist.Main;
+import de.waischbrot.simpletablist.utils.StringUtil;
 import fr.mrmicky.fastboard.FastBoard;
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -14,12 +17,13 @@ public class ScoreboardManager {
 
     private final Main plugin;
     private final Map<UUID, FastBoard> boards;
+    private final List<String> lines;
 
     public ScoreboardManager(Main plugin) {
         this.plugin = plugin;
         boards = new HashMap<>();
 
-
+        lines = plugin.getFileHandler().getConfig().getStringList("scoreboard.lines");
     }
 
     public void addPlayer(Player player) {
@@ -41,12 +45,12 @@ public class ScoreboardManager {
 
     private void updateBoard(FastBoard board) {
 
-        board.updateLines(
-                "",
-                "Players: " + Bukkit.getOnlinePlayers().size(),
-                "",
-                "Kills: " + board.getPlayer().getStatistic(Statistic.PLAYER_KILLS),
-                ""
-        );
+        for (String text : lines) {
+
+            text = StringUtil.getMessageColour(text);
+            text =
+
+        }
+
     }
 }
