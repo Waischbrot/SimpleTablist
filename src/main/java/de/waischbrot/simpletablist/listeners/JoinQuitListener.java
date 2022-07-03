f9a2ce393daa2b53672b4d82f1fdb8870274bda7
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2022-07-03 13:34:18 +0200

    update

--- src/main/java/de/waischbrot/simpletablist/listeners/JoinQuitListener.java
+++ src/main/java/de/waischbrot/simpletablist/listeners/JoinQuitListener.java
@@ -1,4 +1,33 @@
 package de.waischbrot.simpletablist.listeners;
 
-public class JoinQuitListener {
+import de.waischbrot.simpletablist.Main;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinQuitListener implements Listener {
+
+    private final Main plugin;
+
+    public JoinQuitListener(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+        Player player = event.getPlayer();
+        plugin.getScoreboardManager().addPlayer(player);
+
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+
+        Player player = event.getPlayer();
+        plugin.getScoreboardManager().removePlayer(player);
+
+    }
 }
--- src/main/java/de/waischbrot/simpletablist/manager/ScoreboardManager.java
+++ src/main/java/de/waischbrot/simpletablist/manager/ScoreboardManager.java
@@ -1,7 +1,12 @@
 package de.waischbrot.simpletablist.manager;
 
+import de.leonhard.storage.Yaml;
 import de.waischbrot.simpletablist.Main;
+import de.waischbrot.simpletablist.utils.StringUtil;
 import fr.mrmicky.fastboard.FastBoard;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.List;
@@ -10,14 +15,53 @@ import java.util.UUID;
 
 public class ScoreboardManager {
 
-    private final Main plugin;
     private final Map<UUID, FastBoard> boards;
     private final List<String> lines;
 
     public ScoreboardManager(Main plugin) {
-        this.plugin = plugin;
         boards = new HashMap<>();
 
-        lines = plugin.getFileHandler().getConfig()
+        Yaml config = plugin.getFileHandler().getConfig();
+
+        lines = config.getStringList("scoreboard.lines");
+
+        int runningDelay = config.getOrSetDefault("scoreboard.update", 20);
+        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            for (FastBoard board : boards.values()) {
+                updateBoard(board);
+            }
+        }, 0, runningDelay);
+    }
+
+    public void addPlayer(Player player) {
+
+        FastBoard board = new FastBoard(player);
+        board.updateTitle("");
+        boards.put(player.getUniqueId(), board);
+
+    }
+
+    public void removePlayer(Player player) {
+
+        FastBoard board = boards.remove(player.getUniqueId());
+        if (board != null) {
+            board.delete();
+        }
+
+    }
+
+    private void updateBoard(FastBoard board) {
+
+        List<String> newLines = lines;
+
+        for (String text : newLines) {
+
+            text = PlaceholderAPI.setPlaceholders(board.getPlayer(), text);
+            StringUtil.getMessageColour(text);
+
+        }
+
+        board.updateLines(newLines);
+
     }
 }
