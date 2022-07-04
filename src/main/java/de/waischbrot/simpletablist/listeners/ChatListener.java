808b442c31bfe759028d65bf2ca54018c480daf8
Waischbrot <RubyMC@gmx.de>
2022-07-04 17:26:31 +0200

    noooooooooooooooooooooooooo

--- src/main/java/de/waischbrot/simpletablist/listeners/ChatListener.java
+++ src/main/java/de/waischbrot/simpletablist/listeners/ChatListener.java
@@ -8,7 +8,6 @@ import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.cacheddata.CachedMetaData;
@@ -39,36 +38,7 @@ public class ChatListener implements Listener, ChatRenderer {
     @EventHandler
     public void onChat(AsyncChatEvent event) {
 
-        Player player = event.getPlayer();
-
-        String legacyMessage = PlainTextComponentSerializer.plainText().serialize(event.message());
-        System.out.println(legacyMessage);
-
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            if (legacyMessage.contains("@" + p.getName())) {
-                legacyMessage = legacyMessage.replace('@' + p.getName(), "&b@" + p.getName());
-                p.playSound(Sound.sound(Key.key("block.note_block.chime"), Sound.Source.AMBIENT, 4f, 1f));
-            }
-        }
-
-        CachedMetaData metaData = api.getPlayerAdapter(Player.class).getMetaData(player);
-
-        String prefix = metaData.getPrefix();
-        String suffix = metaData.getSuffix();
-        if (prefix == null) prefix = "";
-        if (suffix == null) suffix = "";
-
-        String mF = format;
-        System.out.println(mF);
-
-        mF = mF.replace("%player%", player.getName());
-        mF = mF.replace("%prefix%", prefix);
-        mF = mF.replace("%suffix%", suffix);
-        mF = mF.replace("%message%", legacyMessage);
-        System.out.println(mF);
-
-        TextComponent message = StringUtil.getAdventureColour(mF);
-        event.message(message);
+        event.renderer(this);
     }
 
     @Override
@@ -100,7 +70,6 @@ public class ChatListener implements Listener, ChatRenderer {
         mF = mF.replace("%message%", legacyMessage);
         System.out.println(mF);
 
-        TextComponent msg = StringUtil.getAdventureColour(mF);
-        return msg;
+        return StringUtil.getAdventureColour(mF);
     }
 }
